25d6559f757e6436f8f647cbd1b847a868ef1775
Hahm <chahm@deloitte.com>
2025-03-25 07:37:05 -0500

    Setup proto files

---
+++ proto/__init__.py
@@ -0,0 +1,2 @@
+from grpc_server_pb2 import *
+from grpc_server_pb2_grpc import *
---
+++ proto/grpc_server_pb2.pyi
@@ -0,0 +1,36 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ShapeType(_message.Message):
+    __slots__ = ("shape_type",)
+    SHAPE_TYPE_FIELD_NUMBER: _ClassVar[int]
+    shape_type: str
+    def __init__(self, shape_type: _Optional[str] = ...) -> None: ...
+
+class ShapeId(_message.Message):
+    __slots__ = ("shape_id",)
+    SHAPE_ID_FIELD_NUMBER: _ClassVar[int]
+    shape_id: int
+    def __init__(self, shape_id: _Optional[int] = ...) -> None: ...
+
+class Shape(_message.Message):
+    __slots__ = ("shape_id", "shape_type", "coords")
+    SHAPE_ID_FIELD_NUMBER: _ClassVar[int]
+    SHAPE_TYPE_FIELD_NUMBER: _ClassVar[int]
+    COORDS_FIELD_NUMBER: _ClassVar[int]
+    shape_id: int
+    shape_type: str
+    coords: _containers.RepeatedCompositeFieldContainer[ShapeCoord]
+    def __init__(self, shape_id: _Optional[int] = ..., shape_type: _Optional[str] = ..., coords: _Optional[_Iterable[_Union[ShapeCoord, _Mapping]]] = ...) -> None: ...
+
+class ShapeCoord(_message.Message):
+    __slots__ = ("x", "y")
+    X_FIELD_NUMBER: _ClassVar[int]
+    Y_FIELD_NUMBER: _ClassVar[int]
+    x: int
+    y: int
+    def __init__(self, x: _Optional[int] = ..., y: _Optional[int] = ...) -> None: ...
